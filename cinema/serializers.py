b32659083e7dd7347f12e3efec215ae5a7ab4c59
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-04-04 16:08:29 +0300

    solution for py-api-views

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import Movie, Genre, Actor, CinemaHall
 
 
 class MovieSerializer(serializers.Serializer):
@@ -21,4 +21,67 @@ class MovieSerializer(serializers.Serializer):
 
         instance.save()
 
+        return instance
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+    name = serializers.CharField(max_length=100)
+
+    def create(self: Genre, validated_data: dict) -> Genre:
+        return Genre.objects.create(**validated_data)
+
+    def update(self: Genre, instance: Genre.objects,
+               validated_data: dict) -> Genre:
+        instance.name = validated_data.get("name", instance.name)
+        instance.save()
+        return instance
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
+
+    def create(self: Actor, validated_data: dict) -> Actor:
+        return Actor.objects.create(**validated_data)
+
+    def update(self: Actor, instance: Actor.objects,
+               validated_data: dict) -> Actor:
+        instance.first_name = validated_data.get(
+            "first_name", instance.first_name)
+        instance.last_name = validated_data.get(
+            "last_name", instance.last_name)
+        instance.save()
+        return instance
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = "__all__"
+
+    name = serializers.CharField(max_length=100)
+    rows = serializers.IntegerField()
+    seats_in_row = serializers.IntegerField()
+
+    def create(self: CinemaHall, validated_data: dict) -> CinemaHall:
+        return CinemaHall.objects.create(**validated_data)
+
+    def update(self: CinemaHall, instance: CinemaHall.objects,
+               validated_data: dict) -> CinemaHall:
+        instance.name = validated_data.get(
+            "name", instance.name)
+        instance.rows = validated_data.get(
+            "rows", instance.rows)
+        instance.seats_in_row = validated_data.get(
+            "seats_in_row", instance.seats_in_row
+        )
+        instance.save()
         return instance
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,34 @@
-from django.urls import path
+from django.urls import path, include
+from cinema.views import (GenreList, GenreDetail,
+                          ActorList, ActorDetail,
+                          CinemaHallViewSet, MovieViewSet)
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+router = routers.DefaultRouter()
+
+router.register("movies", MovieViewSet)
+
+cinema_halls_list = CinemaHallViewSet.as_view(
+    actions={"get": "list", "post": "create"}
+)
+
+cinema_halls_detail = CinemaHallViewSet.as_view(
+    actions={"get": "retrieve",
+             "put": "update",
+             "delete": "destroy",
+             "patch": "partial_update"}
+)
+
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="genres-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genres-detail"),
+    path("actors/", ActorList.as_view(), name="actors-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actors-detail"),
+    path("cinema_halls/", cinema_halls_list, name="cinema_halls-list"),
+    path("cinema_halls/<int:pk>/", cinema_halls_detail,
+         name="cinema_halls-detail"),
+    path("", include(router.urls)),
 ]
 
 app_name = "cinema"
